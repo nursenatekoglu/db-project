0f25e032f55638287f538e48f45b0fe971718ae4
nursena <nursena.tekoglu@gmail.com>
2023-04-04 16:12:32 +0100

    second commmit

--- Queries/HR_8_CREATING_TABLE.sql
+++ Queries/HR_8_CREATING_TABLE.sql
@@ -1,6 +1,7 @@
 /*
 
- create table syntax:
+ how to create table:
+
      create table TableName(
         colName1 DataType Constraints,
         colName2 DataType Constraints(optional),
@@ -22,6 +23,8 @@ SELECT * FROM ScrumTeam;
 
 /*
 
+ how to insert data
+
  INSERT INTO tableName (column1, column2,...)
  VALUES (value1, value2, ...)
 
@@ -35,3 +38,43 @@ INSERT INTO ScrumTeam VALUES (2, 'Joey', 'Tirribiani', 'Developer');
 INSERT INTO ScrumTeam VALUES (3, 'Phoebe', 'Buffay', 'ScrumMaster');
 
 INSERT INTO ScrumTeam VALUES (4, 'Monica', 'Geller', 'PO');
+
+
+
+/*
+
+how to update data
+
+ UPDATE tableName
+ SET column1 = value1,
+     column 2 = value2, ...
+ WHERE condition;
+
+ */
+
+ UPDATE ScrumTeam
+SET JOBTITLE = 'Tester'
+WHERE EMP_ID = 4;
+
+UPDATE ScrumTeam
+SET FIRSTNAME = 'Rachel', LASTNAME='Green'
+WHERE EMP_ID= 1;
+
+/*
+
+ how to DELETE from table
+
+ DELETE FROM tableName
+ WHERE condition;
+
+ */
+
+DELETE FROM ScrumTeam
+WHERE EMP_ID=4;
+
+SELECT * FROM ScrumTeam;
+
+--saving changes
+COMMIT;
+
+
